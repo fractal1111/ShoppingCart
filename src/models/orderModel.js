506357c98b2262d6c7f05a862b07e74fa2f61dab
fractal1111 <parthauto4@gmail.com>
2022-04-19 13:07:04 +0530

    All 4 features compelete

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -1,38 +1,37 @@
 const mongoose = require('mongoose')
 
-const orderSchema = new mongoose.Schema ({
+const orderSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Types.ObjectId,
-        required: 'userId is required',
-        refs: "User",
-        
+        required: [true , 'userId is required'],
+        refs: "User"
     },
 
     items: [{
         productId: {
             type: mongoose.Types.ObjectId,
-            required: 'productId is required',
+            required: [true ,'productId is required'],
             refs: "Product"
         },
         quantity: {
             type: Number,
-            required: "Enter quantity required",
+            required: [true , "Enter quantity required"],
             min: 1
         }
     }],
     totalPrice: {
         type: Number,
-        required: 'totalPrice is required',
+        required: [true ,'totalPrice is required'],
         Comment: 'Holds total price of all the items in the cart'
     },
     totalItems: {
         type: Number,
-        required: 'totalItems are required',
+        required: [true ,'totalItems are required'],
         Comment: 'Holds total number of items in the cart'
     },
     totalQuantity: {
         type: Number,
-        required: 'totalQuantity are required',
+        required: [true ,'totalQuantity are required'],
         Comment: 'Holds total number of items in the cart'
     },
     cancellable: {
@@ -42,9 +41,10 @@ const orderSchema = new mongoose.Schema ({
     status: {
         type: String,
         default: 'pending',
+        enum:['pending', 'completed', 'cancelled']
 
     }
 }, { timestamps: true })
 
 
-module.exports = mongoose.model('Order', orderSchema, 'order')
+module.exports = mongoose.model('Order', orderSchema)
